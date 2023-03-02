item() -> detach() and mixed precision fix

mmdet3d/models/backbones/sst_v1.py
@@ -125,7 +125,7 @@ class SSTv1(nn.Module):
         assert voxel_info['coors'].dtype == torch.int64, 'data type of coors should be torch.int64!'
         self.set_drop_info()
         device = voxel_info['coors'].device
-        batch_size = voxel_info['coors'][:, 0].max().item() + 1
+        batch_size = voxel_info['coors'][:, 0].max().detach() + 1
 
         num_shifts = len(ind_dict_list) # Usually num_shifts == 2, one for non-shifted layout, one for shifted layout
         
@@ -165,7 +165,7 @@ class SSTv1(nn.Module):
     
     def get_key_padding_mask(self, ind_dict, voxel_drop_lvl, device):
         num_all_voxel = len(voxel_drop_lvl)
-        key_padding = torch.ones((num_all_voxel, 1)).to(device).bool()
+        key_padding = torch.ones((num_all_voxel, 1), device=device, dtype=torch.bool)
 
         window_key_padding_dict = flat2window(key_padding, voxel_drop_lvl, ind_dict, self.drop_info)
 

mmdet3d/models/backbones/sst_v2.py
@@ -115,7 +115,7 @@ class SSTv2(nn.Module):
         assert voxel_info['voxel_coors'].dtype == torch.int64, 'data type of coors should be torch.int64!'
 
         device = voxel_info['voxel_coors'].device
-        batch_size = voxel_info['voxel_coors'][:, 0].max().item() + 1
+        batch_size = voxel_info['voxel_coors'][:, 0].max().detach() + 1
         voxel_feat = voxel_info['voxel_feats']
         ind_dict_list = [voxel_info[f'flat2win_inds_shift{i}'] for i in range(num_shifts)]
         padding_mask_list = [voxel_info[f'key_mask_shift{i}'] for i in range(num_shifts)]

mmdet3d/models/middle_encoders/sst_input_layer.py
@@ -137,7 +137,7 @@ class SSTInputLayer(nn.Module):
             if self.debug:
                 assert inner_win_inds.max() < max_tokens, f'Max inner inds({inner_win_inds.max()}) larger(equal) than {max_tokens}'
                 assert (flat2window_inds >= 0).all()
-                max_ind = flat2window_inds.max().item()
+                max_ind = flat2window_inds.max().detach()
                 assert  max_ind < num_windows * max_tokens, f'max_ind({max_ind}) larger than upper bound({num_windows * max_tokens})'
                 assert  max_ind >= (num_windows-1) * max_tokens, f'max_ind({max_ind}) less than lower bound({(num_windows-1) * max_tokens})'
 
@@ -188,7 +188,7 @@ class SSTInputLayer(nn.Module):
             assert (num_tokens_each_win > 0).all()
             random_win = unique_sort_inds[random.randint(0, len(unique_sort_inds)-1)]
             random_mask = win_inds == random_win
-            num_voxel_this_win = bincount[random_win].item()
+            num_voxel_this_win = bincount[random_win].detach()
             random_inner_inds = inner_inds_reorder[random_mask] 
 
             assert len(torch.unique(random_inner_inds)) == num_voxel_this_win
@@ -202,7 +202,7 @@ class SSTInputLayer(nn.Module):
         inner_inds = -torch.ones_like(win_inds)
         for ind in unique_win_inds:
             mask = win_inds == ind
-            num = mask.sum().item()
+            num = mask.sum().detach()
             inner_inds[mask] = torch.arange(num, dtype=win_inds.dtype, device=win_inds.device)
         assert (inner_inds >= 0).all()
         return inner_inds
@@ -340,7 +340,7 @@ class SSTInputLayer(nn.Module):
 
         unique_inds, _ = torch.sort(torch.unique(inds))
         num_valid_inds = len(unique_inds)
-        max_origin_inds = unique_inds.max().item()
+        max_origin_inds = unique_inds.max().detach()
         canvas = -torch.ones((max_origin_inds+1,), dtype=dtype, device=device)
         canvas[unique_inds] = torch.arange(num_valid_inds, dtype=dtype, device=device)
 

mmdet3d/models/middle_encoders/sst_input_layer_v2.py
@@ -282,7 +282,7 @@ class SSTInputLayerV2(nn.Module):
     @torch.no_grad()
     def get_key_padding_mask(self, ind_dict):
         num_all_voxel = len(ind_dict['voxel_drop_level'])
-        key_padding = torch.ones((num_all_voxel, 1)).to(ind_dict['voxel_drop_level'].device).bool()
+        key_padding = torch.ones((num_all_voxel, 1), device=ind_dict['voxel_drop_level'].device, dtype=torch.bool)
 
         window_key_padding_dict = flat2window_v2(key_padding, ind_dict)
 

mmdet3d/models/segmentors/encoder_decoder.py
@@ -259,7 +259,8 @@ class EncoderDecoder3D(Base3DSegmentor):
                 if replace:  # duplicate
                     point_idxs_repeat = point_idxs[torch.randint(
                         0, point_idxs.shape[0],
-                        size=(num_repeat, )).to(device)]
+                        size=(num_repeat, ),
+                        device=device)]
                 else:
                     point_idxs_repeat = point_idxs[torch.randperm(
                         point_idxs.shape[0])[:num_repeat]]

mmdet3d/models/voxel_encoders/utils.py
@@ -19,9 +19,8 @@ def get_paddings_indicator(actual_num, max_num, axis=0):
     # tiled_actual_num: [N, M, 1]
     max_num_shape = [1] * len(actual_num.shape)
     max_num_shape[axis + 1] = -1
-    max_num = torch.arange(max_num, dtype=torch.int, device=actual_num.device).view(
-        max_num_shape
-    )
+    max_num = torch.arange(
+        max_num, dtype=torch.int, device=actual_num.device).view(max_num_shape)
     # tiled_actual_num: [[3,3,3,3,3], [4,4,4,4,4], [2,2,2,2,2]]
     # tiled_max_num: [[0,1,2,3,4], [0,1,2,3,4], [0,1,2,3,4]]
     paddings_indicator = actual_num.int() > max_num
@@ -46,25 +45,23 @@ class VFELayer(nn.Module):
             and pointwise features.
     """
 
-    def __init__(
-        self,
-        in_channels,
-        out_channels,
-        norm_cfg=dict(type="BN1d", eps=1e-3, momentum=0.01),
-        max_out=True,
-        cat_max=True,
-    ):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 norm_cfg=dict(type='BN1d', eps=1e-3, momentum=0.01),
+                 max_out=True,
+                 cat_max=True):
         super(VFELayer, self).__init__()
         self.fp16_enabled = False
         self.cat_max = cat_max
         self.max_out = max_out
         # self.units = int(out_channels / 2)
-        print("create hard")
+        print('create hard')
 
         self.norm = build_norm_layer(norm_cfg, out_channels)[1]
         self.linear = nn.Linear(in_channels, out_channels, bias=False)
 
-    @auto_fp16(apply_to=("inputs"), out_fp32=True)
+    @auto_fp16(apply_to=('inputs'), out_fp32=True)
     def forward(self, inputs):
         """Forward function.
 
@@ -87,7 +84,8 @@ class VFELayer(nn.Module):
         voxel_count = inputs.shape[1]
 
         x = self.linear(inputs)
-        x = self.norm(x.permute(0, 2, 1).contiguous()).permute(0, 2, 1).contiguous()
+        x = self.norm(x.permute(0, 2, 1).contiguous()).permute(0, 2,
+                                                               1).contiguous()
         pointwise = F.relu(x)
         # [K, T, units]
         if self.max_out:
@@ -117,19 +115,18 @@ class DynamicVFELayer(nn.Module):
         norm_cfg (dict): Config dict of normalization layers
     """
 
-    def __init__(
-        self,
-        in_channels,
-        out_channels,
-        norm_cfg=dict(type="BN1d", eps=1e-3, momentum=0.01),
-    ):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 norm_cfg=dict(type='BN1d', eps=1e-3, momentum=0.01)
+                 ):
         super(DynamicVFELayer, self).__init__()
         self.fp16_enabled = False
         # self.units = int(out_channels / 2)
         self.norm = build_norm_layer(norm_cfg, out_channels)[1]
         self.linear = nn.Linear(in_channels, out_channels, bias=False)
 
-    @auto_fp16(apply_to=("inputs"), out_fp32=False)
+    @auto_fp16(apply_to=('inputs'), out_fp32=False)#
     def forward(self, inputs):
         """Forward function.
 
@@ -163,18 +160,16 @@ class PFNLayer(nn.Module):
             Default to 'max'.
     """
 
-    def __init__(
-        self,
-        in_channels,
-        out_channels,
-        norm_cfg=dict(type="BN1d", eps=1e-3, momentum=0.01),
-        last_layer=False,
-        mode="max",
-    ):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 norm_cfg=dict(type='BN1d', eps=1e-3, momentum=0.01),
+                 last_layer=False,
+                 mode='max'):
 
         super().__init__()
         self.fp16_enabled = False
-        self.name = "PFNLayer"
+        self.name = 'PFNLayer'
         self.last_vfe = last_layer
         if not self.last_vfe:
             out_channels = out_channels // 2
@@ -183,10 +178,10 @@ class PFNLayer(nn.Module):
         self.norm = build_norm_layer(norm_cfg, self.units)[1]
         self.linear = nn.Linear(in_channels, self.units, bias=False)
 
-        assert mode in ["max", "avg"]
+        assert mode in ['max', 'avg']
         self.mode = mode
 
-    @auto_fp16(apply_to=("inputs"), out_fp32=True)
+    @auto_fp16(apply_to=('inputs'), out_fp32=True)
     def forward(self, inputs, num_voxels=None, aligned_distance=None):
         """Forward function.
 
@@ -203,25 +198,24 @@ class PFNLayer(nn.Module):
             torch.Tensor: Features of Pillars.
         """
         x = self.linear(inputs)
-        x = self.norm(x.permute(0, 2, 1).contiguous()).permute(0, 2, 1).contiguous()
+        x = self.norm(x.permute(0, 2, 1).contiguous()).permute(0, 2,
+                                                               1).contiguous()
         x = F.relu(x)
 
-        if self.mode == "max":
+        if self.mode == 'max':
             if aligned_distance is not None:
                 x = x.mul(aligned_distance.unsqueeze(-1))
             x_max = torch.max(x, dim=1, keepdim=True)[0]
-        elif self.mode == "avg":
+        elif self.mode == 'avg':
             if aligned_distance is not None:
                 x = x.mul(aligned_distance.unsqueeze(-1))
-            x_max = x.sum(dim=1, keepdim=True) / num_voxels.type_as(inputs).view(
-                -1, 1, 1
-            )
+            x_max = x.sum(
+                dim=1, keepdim=True) / num_voxels.type_as(inputs).view(
+                    -1, 1, 1)
 
         if self.last_vfe:
             return x_max
         else:
             x_repeat = x_max.repeat(1, inputs.shape[1], 1)
-            x_concatenated = torch.cat([x, x_repeat], dim=2)
-            return x_concatenated
             x_concatenated = torch.cat([x, x_repeat], dim=2)
             return x_concatenated

mmdet3d/ops/sst/sst_ops.py
@@ -53,7 +53,7 @@ def get_flat2win_inds(batch_win_inds, voxel_drop_lvl, drop_info, debug=True):
         if debug:
             assert inner_win_inds.max() < max_tokens, f'Max inner inds({inner_win_inds.max()}) larger(equal) than {max_tokens}'
             assert (flat2window_inds >= 0).all()
-            max_ind = flat2window_inds.max().item()
+            max_ind = flat2window_inds.max().detach()
             assert  max_ind < num_windows * max_tokens, f'max_ind({max_ind}) larger than upper bound({num_windows * max_tokens})'
             assert  max_ind >= (num_windows-1) * max_tokens, f'max_ind({max_ind}) less than lower bound({(num_windows-1) * max_tokens})'
 
@@ -88,7 +88,7 @@ def flat2window(feat, voxel_drop_lvl, flat2win_inds_dict, drop_info):
         this_inds = flat2win_inds_dict[dl][0]
 
         max_tokens = drop_info[dl]['max_tokens']
-        num_windows = (this_inds // max_tokens).max().item() + 1
+        num_windows = (this_inds // max_tokens).max().detach() + 1
         feat_3d = torch.zeros((num_windows * max_tokens, feat_dim), dtype=dtype, device=device)
         if this_inds.max() >= num_windows * max_tokens:
             set_trace()
@@ -185,7 +185,7 @@ def get_inner_win_inds(win_inds):
     assert (num_tokens_each_win > 0).all()
     random_win = unique_sort_inds[random.randint(0, len(unique_sort_inds)-1)]
     random_mask = win_inds == random_win
-    num_voxel_this_win = bincount[random_win].item()
+    num_voxel_this_win = bincount[random_win].detach()
     random_inner_inds = inner_inds_reorder[random_mask] 
 
     assert len(torch.unique(random_inner_inds)) == num_voxel_this_win
@@ -253,7 +253,7 @@ def make_continuous_inds(inds):
 
     unique_inds, _ = torch.sort(torch.unique(inds))
     num_valid_inds = len(unique_inds)
-    max_origin_inds = unique_inds.max().item()
+    max_origin_inds = unique_inds.max().detach()
     canvas = -torch.ones((max_origin_inds+1,), dtype=dtype, device=device)
     canvas[unique_inds] = torch.arange(num_valid_inds, dtype=dtype, device=device)
 
@@ -503,7 +503,7 @@ class SRATensor(object):
 
             assert inner_win_inds.max() < max_tokens, f'Max inner inds({inner_win_inds.max()}) larger(equal) than {max_tokens}'
             assert (flat2window_inds >= 0).all()
-            max_ind = flat2window_inds.max().item()
+            max_ind = flat2window_inds.max().detach()
             assert  max_ind < num_windows * max_tokens, f'max_ind({max_ind}) larger than upper bound({num_windows * max_tokens})'
             assert  max_ind >= (num_windows-1) * max_tokens, f'max_ind({max_ind}) less than lower bound({(num_windows-1) * max_tokens})'
 
@@ -649,7 +649,7 @@ class SRATensor(object):
     def get_key_padding_mask(self, transform_info, voxel_drop_level, batching_info, device):
 
         num_all_voxel = len(voxel_drop_level)
-        key_padding = torch.ones((num_all_voxel, 1)).to(device).bool()
+        key_padding = torch.ones((num_all_voxel, 1), device=device, dtype=torch.bool)
 
         window_key_padding_dict = flat2window(key_padding, voxel_drop_level, transform_info, batching_info)
 